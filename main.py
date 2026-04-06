80823a433459d313a2ec5ab1e59a1c26c042895d
Vineet Posani <vineetposani97@gmail.com>
2026-04-06 18:59:01 +0530

    fixed syntax error fully

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ Workflow:
 """
 
 response = client.chat.completions.create(
-model="gpt-4.1-mini",
-messages=[{"role": "user", "content": prompt}]
+    model="gpt-4.1-mini",
+    messages=[{"role": "user", "content": prompt}]
 )
 
 return response.choices[0].message.content
